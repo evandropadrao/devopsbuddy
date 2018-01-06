f6cadcfd2ffb5b05aec87ffe154ef88b1e764abf
evandropadrao <evandropadrao@gmail.com>
2018-01-06 00:46:14 -0200

    Added logging setup with logback

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,6 +1,8 @@
 package com.devopsbuddy.web.i18n;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -11,6 +13,9 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /* The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
@@ -19,6 +24,9 @@ public class I18NService {
      * @param messageId The key to the message resource file
      */
     public String getMessage(String messageId) {
+
+        LOG.info("Returning i18n text for messageId {}", messageId);
+
         Locale locale = LocaleContextHolder.getLocale();
 
         return getMessage(messageId, locale);
